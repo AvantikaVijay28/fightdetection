bdd23b5160f2b30a282b5f06c4118ca944b70372
AvantikaVijay28 <avantikavijay0@gmail.com>
2026-03-29 18:11:11 +0530

    env activation

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# Ensure pip, setuptools, wheel are up-to-date
+# Exit immediately if a command fails
+set -e
+
+# Activate Render's virtual environment
+source .venv/bin/activate
+
+# Upgrade core Python packaging tools
 python -m pip install --upgrade pip setuptools wheel
 
-# Install all packages from requirements.txt into the Render virtual environment
+# Install all packages from requirements.txt
 pip install -r requirements.txt
 
+# Ensure Gunicorn is installed
 pip install gunicorn
